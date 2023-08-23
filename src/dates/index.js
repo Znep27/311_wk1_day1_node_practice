ca341e893d8a5eb36a411e9a19deeb20c7999f2a
Marc Rozenburgh <znep27@hotmail.com>
2023-08-22 20:34:38 -0500

    dates completed

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,20 @@
 // import moment here; use this package in each function
+var moment = require('moment'); // require
+moment().format(); 
 
 const today = () => {
   // write code for dates.today
-
+  return moment().format("dddd")
 }
 
 const calendar = () => {
   // write code for dates.calendar
-
+  return moment().format("MMM D, YYYY")
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+  return moment().format("hh:mm:ss A")
 }
 
 module.exports = {
