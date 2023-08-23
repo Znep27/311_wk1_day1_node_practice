4d2a928f5e58b9ba1df390eb2b5094d11cc27716
Marc Rozenburgh <znep27@hotmail.com>
2023-08-22 20:07:29 -0500

    strings complete

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,26 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  const arr = str.split(delim)
+  return arr
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let pairArr = []
+  let a = 0
+  for (let i = 0; i < str.length; i = i + 2) {
+    pairArr[a] = str.slice(i, i + 2)
+    a++
+  }
+  return pairArr
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let reverseArr = str.split('')
+  reverseArr = reverseArr.reverse()
+  str = reverseArr.join('')
+  return str
 }
 
 module.exports = {
